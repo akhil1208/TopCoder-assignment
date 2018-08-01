e451d3882cfd256763e53df52c85026728b42fc9
akhil1208 <41956153+akhil1208@users.noreply.github.com>
2018-08-01 11:04:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require ("express");
 var app = express();
 var port = process.env.PORT || 3000;
-var s=10;
-var t=20;
+
 app.get("/", function(req,res) {
-	res.send(s+t);
+	res.send("HELLO WORLD");
 });
 
 app.listen(port);
